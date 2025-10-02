759bf79eddca2a8a3ec051c55139e596d2768b93
Priyasha Haldar <priyashahaldar2005@gmail.com>
2025-10-03 00:18:30 +0530

    commit

---
+++ 1.js
@@ -0,0 +1,14 @@
+/* Data types
+    undefined, null, boolean, string, symbol, number and object
+*/
+
+var Name="tithi"
+Name=19
+let ourname="geekforgeeks"
+const date=25
+
+var a;
+var b = 2;
+a=7;
+b=a;
+console.log(a)
